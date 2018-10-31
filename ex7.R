edd13c5d5673e5644148a1d51228928fdde9e953
Brittni Bertolet <bbertole@nd.edu>
2018-10-31 12:59:04 -0400

    Update ex7.R

--- ex7.R
+++ ex7.R
@@ -9,7 +9,7 @@ file = read.table('iris.csv',header=TRUE,sep=",",stringsAsFactors=FALSE)
 OnlyOdds = function(DataFrame){
   NumRows = nrow(DataFrame)
   OddIndexes = seq(1, NumRows, 2)
-  file[OddIndexes,]
+  file[OddIndexes,] #Brittni: Because you use "file", this makes it not flexible and ignores the argument DataFrame. A more flexible command would be DataFrame[OddIndexes,]. (-0.25)
 }
 OnlyOdds(file)
 
@@ -31,7 +31,7 @@ SepalWidth(3.5)
 #2C.Function writes data for a given species to a comma-delimited file
 NewFile = function(Flower){
   FlowerData = subset(file, Species ==Flower)
-  FileName = paste(Flower,".csv")
+  FileName = paste(Flower,".csv") #Brittni: This makes a FileName with a space in it, because the default seperator of paste() is a space. Use sep="" to not have a space. (-0.25)
   write.csv(FlowerData, file = FileName)
 }
 NewFile('setosa')
